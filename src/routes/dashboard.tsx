80c737bf9bce911498c5133d85f99457d2322a61
timperumusanbidiy <fitriandaayu98@gmail.com>
2026-09-02 08:50:37 +0700

    Update dashboard.tsx

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -88,7 +88,7 @@ export const Route = createFileRoute("/dashboard")({
 });
 
 // Ganti ini kalau backend jalan di alamat/port lain.
-const API_BASE = "https://sentimenanalisis-production-bf9a.up.railway.app/";
+const API_BASE = "https://sentimenanalisis-production.up.railway.app/";
 
 const BRAND = {
   p50: "#EEF5FF",
